bbca5fb846667093d58db29ec7dd6d02ba6515e3
Eemil Hietava <Eemil.Hietava@student.lab.fi>
2025-04-09 10:09:48 +0300

    valmis

--- java/scripts/calculator.js
+++ java/scripts/calculator.js
@@ -1,3 +1,5 @@
+let calculationCount = 0;
+
 window.onload = function () {
     document.getElementById("num1").value = getRandomInt(10);
     document.getElementById("num2").value = getRandomInt(10);
@@ -10,7 +12,10 @@ console.log(getRandomInt(11));
 
 function changeNumber(elementId, change) {
     let input = document.getElementById(elementId);
-    input.value = parseInt(input.value) + change;
+    let newValue = parseInt(input.value) + change;
+    if (newValue >= 0 && newValue <= 10) {
+        input.value = newValue;
+    }
 }
 function calculate() {
     let operator = document.getElementById("operator").value;
@@ -30,5 +35,13 @@ function calculate() {
     else if (operator == "/") {
         result = parseInt(num1) / parseInt(num2);
     }
-    tulostus.value = parseInt(result);
+    tulostus.value = parseFloat(result);
+
+    const historyList = document.getElementById("history-list");
+    const listItem = document.createElement("ul");
+    listItem.textContent = `${num1} ${operator} ${num2} = ${parseFloat(result)}`;
+    historyList.appendChild(listItem);
+
+    calculationCount++;
+    document.getElementById("calculation-count").innerText = `laskutoimituksia: ${calculationCount} kpl`;
 }
