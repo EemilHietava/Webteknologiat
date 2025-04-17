e69e23fb768cab6adc26999e7be17ebaf6c53d6b
Eemil Hietava <Eemil.Hietava@student.lab.fi>
2025-04-17 12:05:04 +0300

    listgrid

---
+++ java/scripts/listgrid.js
@@ -0,0 +1,13 @@
+function setList() {
+    // document.getElementById("image-container").classList.remove('grid-container');
+    // document.getElementById("image-container").classList.add('list-container');
+    $('#image-container').removeClass('grid-container');
+    $('#image-container').addClass('list-container');
+}
+
+function setGrid() {
+    // document.getElementById("image-container").classList.remove('list-container');
+    // document.getElementById("image-container").classList.add('grid-container');
+    $('#image-container').removeClass('list-container');
+    $('#image-container').addClass('grid-container');
+}
